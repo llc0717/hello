c0819d9445663875bb46dcc09acc866ee7de347b
llc <1531176342@qq.com>
2021-07-09 22:38:30 +0800

    update

--- src/main/java/com/example/hello/controller/Hello.java
+++ src/main/java/com/example/hello/controller/Hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
-    @GetMapping("/hello/")
+    @GetMapping("/hello")
     public String sayHi(String msg){
         return "hello world " + msg;
     }
